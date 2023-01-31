701b7215db7aa01f704c579b7b1073da38542350
Lennart <115736797+lnart@users.noreply.github.com>
2023-01-31 12:38:49 +0100

    import login route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 import express from "express"
 import { config } from "dotenv"
+import routerLogin from './controllers/login.js'
 config()
 
 const app = express()
@@ -14,7 +15,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/cookieshop')
     .then(() => console.log('DATABASE CONNECTED'))
     .catch(error => console.error(error))
 
-
+app.use(routerLogin)
 
 
 app.listen(process.env.PORT, () => {
