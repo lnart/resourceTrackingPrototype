b699c8740efc6eeb74b5c7f4aabb0a3280fa1e1d
Lennart <115736797+lnart@users.noreply.github.com>
2023-01-31 12:11:22 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+import express from "express"
+import { config } from "dotenv"
+config()
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use('/public', express.static('public'));
+
+mongoose.connect('mongodb://127.0.0.1:27017/cookieshop')
+    .then(() => console.log('DATABASE CONNECTED'))
+    .catch(error => console.error(error))
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`STARTED SERVER ON PORT ${process.env.PORT}`)
+})
