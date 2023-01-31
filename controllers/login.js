06effb27a4bbaa262ddecb68d7eadd6c72b2a45b
Lennart <115736797+lnart@users.noreply.github.com>
2023-01-31 12:38:16 +0100

    Import module, write login route, delete lines

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import express from 'express';
+import express, { response } from 'express';
 import { config } from "dotenv";
 import { Router } from "express";
 
@@ -9,7 +9,12 @@ const router = Router()
 router.use(express.json())
 router.use(express.urlencoded({extendet: true}))
 router.use('/public', express.static('public'))
-router.set('views', './views')
-router.set('view engine', 'ejs')
 
 
+router.get('/login', async(req, res) => {
+
+    res.render('login')
+})
+
+
+export default router
