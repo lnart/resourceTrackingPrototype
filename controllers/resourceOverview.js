9f74fffe57e77bd120adf0a815ca0fdbd98b5623
Lennart <115736797+lnart@users.noreply.github.com>
2023-02-02 10:06:36 +0100

    set up new controller

---
+++ controllers/resourceOverview.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import express, { application, request, response } from 'express';
+import { config } from "dotenv";
+import { Router } from "express";
+import {User} from '../models/user.js'
+import {initialize} from './passport-config.js'
+import passport from 'passport'
+import flash from 'express-flash'
+import session from 'express-session'
+import {ConsumptionData} from '../models/consumptionData.js'
+config()
+
+const router = Router()
+
+router.use(express.json())
+router.use(express.urlencoded({extendet: true}))
+router.use('/public', express.static('public'))
+
+router.use(flash())
+router.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}))
+
+initialize(passport, email => {
+    return User.findOne({email: email})
+    id => User.findOne({_id: id})
+})
+
+router.use(passport.initialize())
+router.use(passport.session())
